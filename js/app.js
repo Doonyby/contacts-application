c9bd028d52665a9d61f41947b501188735df92e5
Dan Riding <doonyby@gmail.com>
2016-04-18 14:12:00 -0600

    started js

---
+++ js/app.js
@@ -0,0 +1,20 @@
+var contact = {
+	firstName: $('#firstName').val(),
+	lastName: $('#lastName').val(),
+	phoneNumber: $('#phoneNumber').val(),
+	street: $('#street').val(),
+	city: $('#city').val(),
+	state: $('#state').val(),
+}
+
+$(document).ready(function () {
+
+	var nameLink = contact.firstName + " " + contact.lastName;
+	$('#addButton').click(function () {
+		$('.contact-list')prepend(nameLink);
+	});
+
+	(nameLink).click(function() {
+		$('.contact-display').append(contact);
+	})
+});
