b3dad07126b8fbbbdc1517d64b3ca4b19ab81779
Dan Riding <doonyby@gmail.com>
2016-04-19 19:38:58 -0600

    added address object

--- js/app.js
+++ js/app.js
@@ -1,21 +1,27 @@
 var contact = {
-	firstName: $('#firstName').val(),
-	lastName: $('#lastName').val(),
-	phoneNumber: $('#phoneNumber').val(),
-	street: $('#street').val(),
-	city: $('#city').val(),
-	state: $('#state').val(),
-}
+	firstName: document.getElementById('firstName').value,
+	lastName: document.getElementById('lastName').value,
+	phoneNumber: document.getElementById('phoneNumber').value,
+	location: [address],
+};
+
+var address = {
+	street: document.getElementById('street').value,
+	city: document.getElementById('city').value,
+	state: document.getElementById('state').value,
+};
+
 
 $(document).ready(function () {
 
 	var nameLink = contact.firstName + " " + contact.lastName;
 	$('#addButton').click(function () {
-		console.log(nameLink);
+		console.log(contact.firstName);
 		$('.contact-list').prepend(nameLink);
 	});
 
 	$(nameLink).click(function() {
+		$('.contact-display').empty();
 		$('.contact-display').append(contact);
 	});
 });
